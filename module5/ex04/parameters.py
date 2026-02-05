24aa931ea50b3fa431d6fc94328608cc230343fe
Bryan <89350478+sabahbryan@users.noreply.github.com>
2026-02-05 16:10:59 +0800

    Upload parameters.py
    Completed code.

--- module5/ex04/parameters.py
+++ module5/ex04/parameters.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
 
+import sys
+
+#print(sys.argv)
+
+# start at '1' to ignore "parameters.py"
+i = 1
+
+while i < len(sys.argv):
+    #print(sys.argv[i])
+    i += 1
+
+print("Number of parameters:", len(sys.argv) - 1)
