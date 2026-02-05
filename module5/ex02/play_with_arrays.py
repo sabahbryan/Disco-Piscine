8e7b32594e7dcfab722fc49ac7665d5a3a972480
Bryan <89350478+sabahbryan@users.noreply.github.com>
2026-02-05 16:09:50 +0800

    Tidy up
    Clean up code. Also edited 2nd array name to ensure proper linkage

--- module5/ex02/play_with_arrays.py
+++ module5/ex02/play_with_arrays.py
@@ -2,31 +2,25 @@
 
 #import array
 
-arr = [2, 8, 9, 48, 8, 22, -12, 2]
-
-arr2 = arr.copy()
+arr1 = [2, 8, 9, 48, 8, 22, -12, 2, 3]
+arr2 = arr1.copy()
 
 i = 0
 
-print(arr)
-
-while i < len(arr):
-    arr[i] += 2
+while i < len(arr2):
+    arr2[i] += 2
 #    print("A")
-#    if arr[i] <= 5:
-#        arr.pop(i)
     i += 1
 
-print(arr)
-
 i = 0
 
-while i < len(arr):
+while i < len(arr2):
 #    print("B")
-    if arr[i] <= 5:
+    if arr2[i] <= 5:
 #        print("C")
-        arr.pop(i)
+        arr2.pop(i)
         i = 0
     i += 1
 
-print(arr)
+print(arr1)
+print(arr2)
