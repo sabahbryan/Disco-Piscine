a088dde5337bd7f54c15ef8e40e98f58f863dd09
Bryan <89350478+sabahbryan@users.noreply.github.com>
2026-02-05 17:20:27 +0800

    Upload ex00, ex01, ex02
    Completed code.
    
    ex00: first parameter only
    ex01: uppercase 1st parameter
    ex02: lowercase 2nd parameter

---
+++ module6/ex00/aff_first_param.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import sys
+
+# index starts at '0', the program's name
+print(sys.argv[1])
---
+++ module6/ex01/upcase_it.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+if len(sys.argv) == 2:
+    word = sys.argv[1].upper()
+    print(word)
+else:
+    print("none")
